21ce4add83186aea753609075de659473a357003
jcd <929760274@qq.com>
2023-09-21 11:41:39 +0800

    feat: add reponse

---
+++ response/response.go
@@ -0,0 +1,35 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func BadRequest(c *gin.Context, err error) {
+	c.JSON(http.StatusBadRequest, gin.H{"msg": "bad request, details: " + err.Error()})
+}
+
+func InternalServerError(c *gin.Context, err error) {
+	c.JSON(http.StatusInternalServerError, gin.H{"msg": "internal server error, details: " + err.Error()})
+}
+
+func Unauthorized(c *gin.Context) {
+	c.JSON(http.StatusUnauthorized, gin.H{"msg": "unauthorized"})
+}
+
+func CreateOneOk(c *gin.Context) {
+	c.JSON(http.StatusCreated, gin.H{"msg": "one created"})
+}
+
+func CreateManyOk(c *gin.Context) {
+	c.JSON(http.StatusCreated, gin.H{"msg": "many created"})
+}
+
+func FetchOneOk(c *gin.Context, data any) {
+	c.JSON(http.StatusOK, gin.H{"msg": "ok", "data": data})
+}
+
+func FetchManyOk(c *gin.Context, data []any) {
+	c.JSON(http.StatusOK, gin.H{"msg": "ok", "datas": data})
+}
